fc845c7d5f3a32bdae8996ec7ab2cc18726f7354
Untitled-Name <55985093+Untitled-Name@users.noreply.github.com>
2022-07-29 01:01:09 +0900

    Updated Files
    - Improved transition flow between screens.

---
+++ src/Components/Button.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Button (props){
+    const {buttonText, targetScreen, currentScreen} = props;
+    return (
+        <button className="buttons titleButtons" onClick={() => {
+            props.leaveAnim(currentScreen, targetScreen);
+            }}>
+            <h2 className="buttonText">{buttonText}</h2>
+        </button>
+    )
+}
